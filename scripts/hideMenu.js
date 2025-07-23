67cfeca8ac5b8d29c7db1e514e1da722599fef5b
JhonatanCrruzz <jhonatancrruzz@gmail.com>
2025-07-23 20:55:52 +0200

    se corrigieron errores al modularizar

--- scripts/hideMenu.js
+++ scripts/hideMenu.js
@@ -1,20 +1,41 @@
+// let menuVisible = false;
+// //Función que oculta o muestra el menu
+// function mostrarOcultarMenu(){
+//     if(menuVisible){
+//         document.getElementById("nav").classList ="";
+//         menuVisible = false;
+//     }else{
+//         document.getElementById("nav").classList ="responsive";
+//         menuVisible = true;
+//     }
+// }
+// function seleccionar(){
+//     //oculto el menu una vez que selecciono una opcion
+//     document.getElementById("nav").classList = "";
+//     menuVisible = false;
+// }
+
 let menuVisible = false;
-//Función que oculta o muestra el menu
-function mostrarOcultarMenu(){
-    if(menuVisible){
-        document.getElementById("nav").classList ="";
+
+// Función que oculta o muestra el menú
+function mostrarOcultarMenu() {
+    if (menuVisible) {
+        document.getElementById("nav").classList = "";
         menuVisible = false;
-    }else{
-        document.getElementById("nav").classList ="responsive";
+    } else {
+        document.getElementById("nav").classList = "responsive";
         menuVisible = true;
     }
 }
-function seleccionar(){
-    //oculto el menu una vez que selecciono una opcion
+
+// Función que se ejecuta al seleccionar una opción del menú
+function seleccionar() {
     document.getElementById("nav").classList = "";
     menuVisible = false;
 }
 
+// Exportarlas explícitamente
+export { mostrarOcultarMenu, seleccionar };
 
 
 
--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,14 @@
+// import './activeColorMenu.js';
+// import './dowloadCv.js';
+// import './hideMenu.js';
+// import './resetForm.js';
+
+
+import { mostrarOcultarMenu, seleccionar } from './hideMenu.js';
 import './activeColorMenu.js';
 import './dowloadCv.js';
-import './hideMenu.js';
 import './resetForm.js';
+
+// Asignar funciones globales para que el HTML pueda usarlas
+window.mostrarOcultarMenu = mostrarOcultarMenu;
+window.seleccionar = seleccionar;
